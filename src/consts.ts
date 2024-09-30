7de9a44bec6dee672963b2798f7cb62cde4b7ab1
AbzVirsUMU <2959874980@qq.com>
2024-09-30 22:19:24 +0800

    6

--- src/consts.ts
+++ src/consts.ts
@@ -17,37 +17,37 @@ export const SERVER_URL = "https://kisaragiakari.netlify.app";
 // Menu items for navigation
 export const menuItems = [
   { id: "home", text: "Home", href: "/", svg: "home", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
   {
-    id: "blog",
-    text: "Blogs",
-    href: "/blog",
-    svg: "blog",
-    target: "_self",
-    subItems: [
-      {
-        id: "all",
-        text: "All blogs",
-        href: "/blog",
-        svg: "post",
-        target: "_self",
-      }, // All blog
-      {
-        id: "tech",
-        text: "Tech blogs",
-        href: "/blog/categories/tech",
-        svg: "cube",
-        target: "_self",
-      }, // Technology category
-      {
-        id: "life",
-        text: "Life blogs",
-        href: "/blog/categories/life",
-        svg: "heart",
-        target: "_self",
-      }, // Life category
-    ],
+      id: "blog",
+      text: "Blogs",
+      href: "/blog",
+      svg: "blog",
+      target: "_self",
+      // subItems: [
+        //   {
+            //     id: "all",
+            //     text: "All blogs",
+            //     href: "/blog",
+            //     svg: "post",
+            //     target: "_self",
+            //   }, // All blog
+            //   {
+                //     id: "tech",
+                //     text: "Tech blogs",
+                //     href: "/blog/categories/tech",
+                //     svg: "cube",
+                //     target: "_self",
+                //   }, // Technology category
+                //   {
+                    //     id: "life",
+                    //     text: "Life blogs",
+                    //     href: "/blog/categories/life",
+                    //     svg: "heart",
+                    //     target: "_self",
+                    //   }, // Life category
+                    // ],
   }, // Blog page with sub-items
+  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
   {
     id: "friend",
     text: "Friend",
